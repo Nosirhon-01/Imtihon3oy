94b773c8b5bfb475f8b2831b1158e5ac6495e51c
Nosirhon <yeezyss13@gmail.com>
2026-02-03 10:08:30 +0500

    update checkToken middleware and add postman collection

--- src/middlewares/checkToken.middleware.js
+++ src/middlewares/checkToken.middleware.js
@@ -3,12 +3,11 @@ import CustomError from "../utils/CustomError.js";
 
 export default (req, res, next) => {
   const auth = req.headers.authorization;
-  if (!auth) throw new CustomError("Authorization yo‘q", 401);
+  if (!auth) throw new CustomError("Authorization yoq", 401);
 
   const token = auth.split(" ")[1];
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-  // ixtiyoriy tekshiruvlar
   if (decoded.userAgent && decoded.userAgent !== req.headers["user-agent"]) {
     throw new CustomError("User-Agent mos emas", 401);
   }
